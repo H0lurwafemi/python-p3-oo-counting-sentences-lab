4b7e68bace24d9562f69f66b226a33299bc83a2d
unknown <stephenoluwafemi52@gmail.com>
2023-10-18 06:37:39 +0100

    Done with assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
+# class MyString:
+#   pass
+
 class MyString:
-  pass
+    def __init__(self, value):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            raise ValueError("The value must be a string")
+
+
+class MyString:
+    # ... (constructor)
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+class MyString:
+    # ... (constructor)
+    
+    def is_question(self):
+        return self.value.endswith("?")
+
+
+class MyString:
+    # ... (constructor)
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+class MyString:
+    # ... (constructor)
+
+    def count_sentences(self):
+        sentences = self.value.split(".")  # Split sentences by period
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]  # Remove empty sentences
+        return len(sentences)
